5b7730c47eb809f603062978e19ff2934665e979
Kyle N. Crabtree <kncrabtree@ucdavis.edu>
2017-10-21 14:31:27 -0700

    Remove python2 versions. Update v2 for 2017F

--- pibsolver_v2.py
+++ pibsolver_v2.py
@@ -19,12 +19,10 @@
 # Author: Kyle N. Crabtree
 #
 
-from scipy import linalg
-from scipy import integrate
+import scipy as sp
+import scipy.constants as spc
 import math
 import numpy
-import matplotlib
-matplotlib.use('PDF',warn=False)
 from matplotlib import pyplot as plt
 import datetime
 
@@ -33,22 +31,29 @@ import datetime
 #        VARIABLES         -
 #---------------------------
 
+#equilibrium bond length in Angstrom
+re = 0.96966
+
+#effective mass in kg
+amu = spc.physical_constants['atomic mass constant'][0]
+mass = (1.*16./(1.+16.))*amu
+
 #minimum x value for partice in a box calculation
-xmin = -10.0
+xmin = -1.0+re
 
 #maximum x value for particle in a box calculation
 #there is no limit, but if xmax<xmin, the values will be swapped
 #if xmax = xmin, then xmax will be set to xmin + 1
-xmax = 10.0
+xmax = 1.0+re
 
 #number of grid points at which to calculate integral
 #must be an odd number. If an even number is given, 1 will be added.
 #minimum number of grid points is 3
-ngrid = 251
+ngrid = 501
 
 #number of PIB wavefunctions to include in basis set
 #minimum is 1
-nbasis = 50
+nbasis = 100
 
 #if you want to control the plot ranges, you can do so here
 make_plots = True
@@ -57,18 +62,14 @@ plotymax = 0
 plotxmin = 0
 plotxmax = 0
 
-#the units of energy are hbar*omega, so we'll let hbar equal 1 and calculate omega from mass and fk
-#While mass and hbar are used in the Hamiltonian calculation for all potentials, the units of hbar*omega only apply to the harmonic oscillator
-#For other potentials, let fk=1 and hbar=1
-mass = 1.0
-nubar = 3600.0
-hbar = 1.0
-fk = 1.0
-omega = math.sqrt(fk/mass)
+#dissociation energy in cm-1
+de = 37778.617
 
-#morse potential parameters
-de = 10.0
-alpha = math.sqrt(fk/2.0/de)
+#force constant in N / m
+fk = 774.7188418117737
+
+#angular frequency in rad/s
+omega = numpy.sqrt(fk/mass)
 
 #output file for eigenvalues
 outfile = "eigenvalues.txt"
@@ -90,20 +91,25 @@ def box(x):
 	return 0
 
 #purely harmonic potential
+#energy in cm^-1
+prefactor = 0.5 * fk * 1e-20/(spc.h*spc.c*100.0)
 def harmonic(x):
-	return 0.5*fk*(x**2.)
+	return prefactor*(x-re)*(x-re)
 
 #anharmonic potential
-def anharmonic(x):
-	return 0.5*(x**2.) + 0.05*(x**4.)
+#def anharmonic(x):
+#	return 0.5*(x**2.) + 0.05*(x**4.)
 
-#Morse potential (Note, try a box of -0.4, 5 to begin)
+#Morse potential
+#energy in cm^-1
+#alpha in A^-1
+alpha = math.sqrt(fk/2.0/(de*spc.h*spc.c*100.0))*1e-10
 def morse(x):
-	return de*(1.-math.exp(-alpha*x))**2.
+	return de*(1.-numpy.exp(-alpha*(x-re)))**2.
 
 #double well potential (minima at x = +/-3)
-def doublewell(x):
-	return x**4.-18.*x**2. + 81.
+#def doublewell(x):
+#	return x**4.-18.*x**2. + 81.
 
 #potential function used for this calculation
 def V(x):
@@ -113,17 +119,20 @@ def V(x):
 #   BEGIN CALCULATION   -
 #------------------------
 
-#function to compute normalized PIB wavefunction
-def pib(x,n,L):
-	return math.sqrt(2./L)*math.sin(n*math.pi*x/L)
 
 #verify that inputs are sane
 if xmax==xmin:
-	xmax = xmin+1
+	xmax = xmin+1.
 elif xmax<xmin:
 	xmin,xmax = xmax,xmin
 
 L = xmax - xmin
+
+#function to compute normalized PIB wavefunction
+tl = numpy.sqrt(2./L)
+pixl = numpy.pi/L
+def pib(x,n,L):
+	return tl*math.sin(n*x*pixl)
 
 ngrid = max(ngrid,3)
 
@@ -147,12 +156,13 @@ H = numpy.zeros((nbasis,nbasis))
 #V is defined above, and will be evaluated numerically
 
 #Compute kinetic energy
-#The Kinetic enery operator is T = -hbar/2m d^2/dx^2
-#in the PIB basis, T|phi(i)> = hbar/2m n^2pi^2/L^2 |phi(i)>
-#Therefore <phi(k)|T|phi(i)> = hbar/2m n^2pi^2/L^2 delta_{ki}
+#The Kinetic enery operator is T = -hbar^2/2m d^2/dx^2
+#in the PIB basis, T|phi(i)> = hbar^2/2m n^2pi^2/L^2 |phi(i)>
+#Therefore <phi(k)|T|phi(i)> = hbar^2/2m n^2pi^2/L^2 delta_{ki}
 #Kinetic energy is diagonal
+kepf = spc.hbar*spc.hbar*math.pi**2./(2.*mass*(L*1e-10)*(L*1e-10)*spc.h*spc.c*100)
 for i in range(0,nbasis):
-	H[i,i] += hbar*(i+1.)**2.*math.pi**2./(2.*mass*L**2.)
+    H[i,i] += kepf*(i+1.)*(i+1.)
 
 #now, add in potential energy matrix elements <phi(j)|V|phi(i)>
 #that is, multiply the two functions by the potential at each grid point and integrate
@@ -163,14 +173,14 @@ for i in range(0,nbasis):
 			for k in range(0,ngrid):
 				p = x[k]
 				y[k] += pib(p-xmin,i+1.,L)*V(p)*pib(p-xmin,j+1.,L)
-			H[i,j] += integrate.simps(y,x)
+			H[i,j] += sp.integrate.simps(y,x)
 		else:
 			H[i,j] += H[j,i]
 
 
 #Solve for eigenvalues and eigenvectors
-evalues, evectors = linalg.eigh(H)
-evalues = evalues*nubar
+evalues, evectors = sp.linalg.eigh(H)
+evalues = evalues
 
 #get ending time
 endtime = datetime.datetime.now()
@@ -179,15 +189,24 @@ endtime = datetime.datetime.now()
 #    GENERATE OUTPUT    -
 #------------------------
 
-print evalues
+print("Results:")
+print("-------------------------------------")
+print("   v  Energy (cm-1)  Delta E (cm-1)  ")
+print("-------------------------------------")
+for i in range(min(40,len(evalues))):
+    if i>0:
+        deltae = evalues[i] - evalues[i-1]        
+        print(" {:>3d}  {:>13.3f}  {:>14.3f}  ".format(i,evalues[i],deltae))
+    else:
+        print(" {:>3d}  {:>13.3f}          ------ ".format(i,evalues[i]))
 
 with open(outfile,'w') as f:
 	f.write(str('#pibsolver.py output\n'))
 	f.write('#start time ' + starttime.isoformat(' ') + '\n')
 	f.write('#end time ' + endtime.isoformat(' ') + '\n')
 	f.write(str('#elapsed time ' + str(endtime-starttime) + '\n'))
-	f.write(str('#xmin {0}\n').format(xmin))
-	f.write(str('#xmax {0}\n').format(xmax))
+	f.write(str('#xmin {:1.4f}\n').format(xmin))
+	f.write(str('#xmax {:1.4f}\n').format(xmax))
 	f.write(str('#grid size {0}\n').format(ngrid))
 	f.write(str('#basis size {0}\n\n').format(nbasis))
 	f.write(str('#eigenvalues\n'))
@@ -211,7 +230,7 @@ if make_plots == True:
     
     plt.figure(1)
     #Make graph of eigenvalue spectrum
-    title = "EV Spectrum, Min={0}, Max={1}, Grid={2}, Basis={3}".format(xmin,xmax,ngrid,nbasis)
+    title = "EV Spectrum, Min={:1.4f}, Max={:1.4f}, Grid={:3d}, Basis={:3d}".format(xmin,xmax,ngrid,nbasis)
     
     plt.plot(evalues,'ro')
     plt.xlabel('v')
@@ -222,10 +241,10 @@ if make_plots == True:
     
     #Make graph with potential and eigenfunctions
     plt.figure(2)
-    title = "Wfns, Min={0}, Max={1}, Grid={2}, Basis={3}".format(xmin,xmax,ngrid,nbasis)
+    title = "Wfns, Min={:1.4f}, Max={:1.4f}, Grid={:3d}, Basis={:3d}".format(xmin,xmax,ngrid,nbasis)
     vplot = numpy.zeros(x.size)
     for i in range(0,x.size):
-        vplot[i] = V(x[i])*nubar
+        vplot[i] = V(x[i])
     plt.plot(x,vplot)
     
     if plotxmin == 0:
@@ -237,12 +256,13 @@ if make_plots == True:
     if(plotxmin > plotxmax):
     	plotxmin, plotxmax = plotxmax,plotxmin
     if plotymax == 0:
-    	plotymax = 1.25*de*nubar
+    	plotymax = 1.25*de
 #          plotymax = evalues[10]
     if(plotymin > plotymax):
     	plotymin, plotymax = plotymax,plotymin
-    
-    sf = evalues[1]-evalues[0]
+    sf = 1.0
+    if len(evalues)>2:
+        sf = (evalues[2]-evalues[0])/5.0
     for i in range(0,nbasis):
     	plt.plot([xmin,xmax],[evalues[i],evalues[i]],'k-')
     for i in range(0,nbasis):
@@ -257,7 +277,7 @@ if make_plots == True:
     plt.plot([xmax,xmax],[plotymin,plotymax],'k-')
     plt.axis([plotxmin,plotxmax,plotymin,plotymax])
     plt.title(title)
-    plt.xlabel('$x-x_0$ $(\sqrt{\hbar/m\omega})$')
+    plt.xlabel('$R$ (Angstrom)')
     plt.ylabel(r'V (cm$^{-1}$)')
     plt.savefig(potentialfile)
     plt.show()
